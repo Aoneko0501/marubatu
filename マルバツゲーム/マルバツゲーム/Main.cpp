64778417ce62da44b93306f8e484db2b01ded541
T K <blue.bit0501@gmail.com>
2016-05-12 22:51:37 +0900

    kousin

--- マルバツゲーム/マルバツゲーム/Main.cpp
+++ マルバツゲーム/マルバツゲーム/Main.cpp
@@ -11,18 +11,18 @@
 
 using namespace std;
 
-enum State{
+enum State {
 	NONE, //空マス(0)
 	MARU, //プレイヤー(1)
 	BATSU //敵マス(2)
 };
 
-enum Turn{
+enum Turn {
 	PLAYER,
 	ENEMY
 };
 
-struct Board{
+struct Board {
 public:
 	void Clear(); //盤面を初期化
 	void Show(); //盤面を表示
@@ -35,7 +35,7 @@ protected:
 
 void PlayerTurn(); //プレイヤー
 void EnemyTurn();//敵
-void gameResult(); //結果発表
+void gameResult(int res); //結果発表
 
 bool bGameSet = false;
 static Turn T = PLAYER; //プレイヤー,COM
@@ -44,20 +44,20 @@ bool is_End = true;
 
 Board board;
 
-int main(void){
+int main(void) {
 	// 盤面を初期化する
 	board.Clear();
 
 	//勝敗が決まるまで無限ループ
-	do{
+	do {
 		PlayerTurn();
-		//	if (board.Check()){ bGameSet = true;  break; }
+			if (board.Check() != 0){ bGameSet = true;  break; }
 		EnemyTurn();
-		//	if (board.Check()){ bGameSet = true; break; }
+			if (board.Check() != 0){ bGameSet = true; break; }
 	} while (!bGameSet);
 
-	//gameResult(); //結果発表
-
+	gameResult(board.Check()); //結果発表
+	board.Show();
 	return 0;
 }
 
@@ -70,20 +70,20 @@ void PlayerTurn()
 	board.Show();
 
 	cin >> nSelect;
-	while (nSelect <= 0 || nSelect >= 10){
+	while (nSelect <= 0 || nSelect >= 10) {
 		nSelect = 0;
 		cout << "\a(1~9)のいずれかを入力してください。" << endl;
 		cin >> nSelect;
 	}
 
-	while (board.Set(nSelect, T) == 1){
+	while (board.Set(nSelect, T) == 1) {
 		cout << "1~9のいずれかを入力してください。" << endl;
 		cin >> nSelect;
 	}
 }
 
 //COM側の処理
-void EnemyTurn(){
+void EnemyTurn() {
 	T = ENEMY;
 	int nSelect = 5;
 	cout << "COMのターンです。" << endl;
@@ -93,18 +93,18 @@ void EnemyTurn(){
 }
 
 //盤面を初期化する
-void Board::Clear(){
+void Board::Clear() {
 	string s;
-	for (int i = 0; i < 9; i++){
+	for (int i = 0; i < 9; i++) {
 		nBoard[i] = NONE;
 	}
 }
 
 //盤面を表示
-void Board::Show(){
+void Board::Show() {
 	string s;
-	for (int i = 0; i < 9; i++){
-		switch (nBoard[i]){
+	for (int i = 0; i < 9; i++) {
+		switch (nBoard[i]) {
 		case NONE:
 			s = to_string(i + 1);
 			strcpy(&cBoard[i], s.c_str());
@@ -129,18 +129,125 @@ void Board::Show(){
 //n番目にセットする。
 int Board::Set(int n, Turn turn)
 {
-	
 
-	if (nBoard[n - 1] == MARU || nBoard[n - 1] == BATSU){
+
+	if (nBoard[n - 1] == MARU || nBoard[n - 1] == BATSU) {
 		cout << "残念ながらそこには置けません。" << endl;
 		return 1;
 	}
 
-	if (T == PLAYER){ //プレイヤー
+	if (T == PLAYER) { //プレイヤー
 		nBoard[n - 1] = MARU;
 	}
-	else{  //COM
+	else {  //COM
 		nBoard[n - 1] = BATSU;
 	}
 	return 0;
+}
+
+//勝敗判定
+int Board::Check()
+{
+	int point = 0;
+	int mark = 0;
+
+	//横判定
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			if (cBoard[mark] == 'o') {
+				point++;
+				if (point == 3) return 1;
+				mark++;
+			}
+			else if (cBoard[mark] == 'x') {
+				point = point + 2;
+				if (point == 6) return 2;
+				mark++;
+			}
+			else {
+				point = 0;
+				mark++;
+			}
+			printf("%d\n", point);
+		}
+		point = 0;
+	}
+	point = 0;
+	mark = 0;
+
+	//縦判定
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			if (cBoard[mark] == 'o') {
+				point++;
+				if (point == 3) return 1;
+				mark = mark + 3;
+			}
+			else if (cBoard[mark] == 'x') {
+				point = point + 2;
+				if (point == 6) return 2;
+				mark = mark + 3;
+			}
+			else {
+				point = 0;
+				mark = mark + 3;
+			}
+			mark = j;
+		}
+	}
+	point = 0;
+	mark = 0;
+
+	// 斜め右下
+	for (int i = 0; i < 3; i++) {
+		if (cBoard[mark] == 'o') {
+			point++;
+			if (point == 3) return 1;
+			mark = mark + 4;
+		}
+		else if (cBoard[mark] == 'x') {
+			point = point + 2;
+			if (point == 6) return 2;
+			mark = mark + 4;
+		}
+		else {
+			point = 0;
+			mark = mark + 4;
+		}
+	}
+
+	point = 0;
+	mark = 2;
+	// 斜め右上
+	for (int i = 0; i < 3; i++) {
+		if (cBoard[mark] == 'o') {
+			point++;
+			if (point == 3) return 1;
+			mark = mark + 2;
+		}
+		else if (cBoard[mark] == 'x') {
+			point = point + 2;
+			if (point == 6) return 2;
+			mark = mark + 2;
+		}
+		else {
+			point = 0;
+			mark = mark + 4;
+		}
+	}
+
+	return 0;
+}
+
+void gameResult(int res) {
+	
+	if (res == 1) {
+		printf("プレイヤーの勝利です。\n");
+	}
+	else if(res == 2){
+		printf("COMの勝利です。\n");
+	}
+	else {
+		printf("引き分けです。\n");
+	}
 }
